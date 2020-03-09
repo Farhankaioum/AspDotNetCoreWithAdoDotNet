1a5d4068ab8c80a6aa21626e7fcf28bfa66ee0cf
FarhanKaioum <km1472233@gmail.com>
2020-03-10 01:37:42 +0600

    Another WPF core project template added

--- KSPStore/WPFTemplateProject/MainWindow.xaml.cs
+++ KSPStore/WPFTemplateProject/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFProjectTemplate
+namespace WPFTemplateProject
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
