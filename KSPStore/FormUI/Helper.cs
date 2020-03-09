9ceb26e2e7bd568976e6a4a811424f40400909e4
FarhanKaioum <km1472233@gmail.com>
2020-03-09 15:16:30 +0600

    Windows Form with Dapper

---
+++ KSPStore/FormUI/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormUI
+{
+    public static class Helper
+    {
+        public static string CnnVal(string connectionName)
+        {
+            return "";
+        }
+    }
+}
---
+++ KSPStore/FormUI/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormUI
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
